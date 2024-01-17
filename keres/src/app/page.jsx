00490abfed9f4ec9bad444694790785bc2fd5177
JRW <jakub.robert.w@gmail.com>
2024-01-17 14:38:04 +0100

    Mobile version only

--- keres/src/app/page.jsx
+++ keres/src/app/page.jsx
@@ -1,16 +1,42 @@
 'use client'
 
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
 import TitlePage from './components/Titlepage'
-
 import Navbar from './components/Navigation'
 
-export default function Home() {
+const Home = () => {
+	const [windowWidth, setWindowWidth] = useState(0)
+
+	useEffect(() => {
+		const setupBeforeRender = () => {
+			console.log('Operacje przed renderem')
+		}
+
+		setupBeforeRender()
+		const handleResize = () => {
+			setWindowWidth(window.innerWidth)
+		}
+
+		handleResize()
+		window.addEventListener('resize', handleResize)
+
+		return () => {
+			window.removeEventListener('resize', handleResize)
+		}
+	}, [])
+
 	return (
 		<main className='h-screen'>
-			<Navbar />
-			<TitlePage />
+			{windowWidth > 700 ? (
+				<p>otwórz na urządzeniu mobilnym</p>
+			) : (
+				<>
+					<Navbar />
+					<TitlePage />
+				</>
+			)}
 		</main>
 	)
 }
+
+export default Home
